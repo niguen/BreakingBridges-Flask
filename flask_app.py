1ea778108af499372b871885c12bdf5b86c31955
Niklas Günther <niklasguenther@outlook.com>
2024-01-21 18:05:06 +0100

    style updated + flas_app script added

---
+++ flask_app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from config import Config
+# from models_sqlite import db
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+
+    #conf = Config()
+    app.config.from_object(config_class)
+
+    # initialize the app with the extension
+    #db = SQLAlchemy(app)
+    # db.init_app(app)    
+
+    # Initialize Flask extensions here
+
+    # Register blueprints here
+    from main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+
+    from admin import bp as admin_bp
+    app.register_blueprint(admin_bp)
+
+    return app
+
+app = create_app()
